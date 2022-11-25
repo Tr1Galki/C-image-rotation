9d8afab3f795e4190f4efc46f1ec625b9bc47538
Max <kalabokmaksim@gmail.com>
2022-11-25 20:48:38 +0300

    mem

--- solution/src/main.c
+++ solution/src/main.c
@@ -2,6 +2,10 @@
 #include "bmp.h"
 #include "file-manager.h"
 
+int f() {
+
+}
+
 int main( int argc, char** argv ) {
     (void) argc; (void) argv; // supress 'unused parameters' warning
     printf("123");
@@ -11,9 +15,8 @@ int main( int argc, char** argv ) {
     struct bmp_header my_header;
     int b = (int) read_header(file, &my_header);
     printf("\nENUM VALUE:%d ",b);
+    printf("aboba");
     
 
-
-    
     return 0;
 }
